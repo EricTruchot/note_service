5b141e1f31d6b607ac21f9ee9a13f0f4aada6fc6
EricTruchot <eric.truchot12@gmail.com>
2023-11-03 15:22:13 +0100

    modify data from db

--- src/main/java/com/noteverse/note_service/api/model/Author.java
+++ src/main/java/com/noteverse/note_service/api/model/Author.java
@@ -2,47 +2,47 @@ package com.noteverse.note_service.api.model;
 
 public class Author {
 
-    private int id;
-    private String name;
-    private String creationDate;
-    private String updatedDate;
+    private int id_author;
+    private String name_author;
+    private String creationDate_author;
+    private String updatedDate_author;
 
-    public Author(int id, String name, String creationDate, String updatedDate) {
-        this.id = id;
-        this.name = name;
-        this.creationDate = creationDate;
-        this.updatedDate = updatedDate;
+    public Author(int id_author, String name_author, String creationDate, String updatedDate) {
+        this.id_author = id_author;
+        this.name_author = name_author;
+        this.creationDate_author = creationDate;
+        this.updatedDate_author = updatedDate;
     }
 
     public int getId() {
-        return id;
+        return id_author;
     }
 
-    public void setId(int id) {
-        this.id = id;
+    public void setId(int id_author) {
+        this.id_author = id_author;
     }
 
     public String getName() {
-        return name;
+        return name_author;
     }
 
-    public void setName(String name) {
-        this.name = name;
+    public void setName(String name_author) {
+        this.name_author = name_author;
     }
 
     public String getCreationDate() {
-        return creationDate;
+        return creationDate_author;
     }
 
-    public void setCreationDate(String creationDate) {
-        this.creationDate = creationDate;
+    public void setCreationDate(String creationDate_author) {
+        this.creationDate_author = creationDate_author;
     }
 
     public String getUpdatedDate() {
-        return updatedDate;
+        return updatedDate_author;
     }
 
-    public void setUpdatedDate(String updatedDate) {
-        this.updatedDate = updatedDate;
+    public void setUpdatedDate(String updatedDate_author) {
+        this.updatedDate_author = updatedDate_author;
     }
 }
--- src/main/java/com/noteverse/note_service/api/model/Note.java
+++ src/main/java/com/noteverse/note_service/api/model/Note.java
@@ -2,37 +2,37 @@ package com.noteverse.note_service.api.model;
 
 public class Note {
 
-    private int id;
-    private String title;
-    private String description;
+    private int id_note;
+    private String title_author;
+    private String description_author;
 
-    public Note(int id, String title, String description) {
-        this.id = id;
-        this.title = title;
-        this.description = description;
+    public Note(int id_note, String title_author, String description_author) {
+        this.id_note = id_note;
+        this.title_author = title_author;
+        this.description_author = description_author;
     }
 
     public int getId() {
-        return id;
+        return id_note;
     }
 
-    public void setId(int id) {
-        this.id = id;
+    public void setId(int id_note) {
+        this.id_note = id_note;
     }
 
     public String gettitle() {
-        return title;
+        return title_author;
     }
 
-    public void settitle(String title) {
-        this.title = title;
+    public void settitle(String title_author) {
+        this.title_author = title_author;
     }
 
     public String getdescription() {
-        return description;
+        return description_author;
     }
 
-    public void setdescription(String description) {
-        this.description = description;
+    public void setdescription(String description_author) {
+        this.description_author = description_author;
     }
 }
